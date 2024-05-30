c9e56288a84e1734890349292672800e13c8a441
rafael <rafael.florianoc9@gmail.com>
2024-05-30 11:56:36 -0300

    FEAT: adding more mocked records

--- LanchesSenac/src/Pages/Funcionario/Funcionario.tsx
+++ LanchesSenac/src/Pages/Funcionario/Funcionario.tsx
@@ -1,34 +1,63 @@
+import './Funcionario.css';
 import { Col, Container, ListGroup, Row } from "react-bootstrap";
 
 
 const Funcionario = () => {
     return (
-        <div>
-            <Container>
-                <Row>
-                    <Col smsm={4} >
-                        <label>Id</label>
-                    </Col>
-                    <Col>
-                        <label>Nome</label>
-                    </Col>
-                    <Col>
-                        <label>Cargo</label>
-                    </Col>
-                </Row>
-            </Container>
-            <Container>
-                <Row>
-                    <Col sm={4} >
-                        <label>123</label>
-                    </Col>
-                    <Col>
-                        <label>Rafael Floriano</label>
-                    </Col>
-                    <Col>
-                        <label>Programador</label>
-                    </Col>
-                </Row>
+        <div className="stack-of-records">
+            <Container className='header-of-records'>
+                <Container>
+                    <Row className='header-row-records'>
+                        <Col smsm={4} >
+                            <label>Id</label>
+                        </Col>
+                        <Col>
+                            <label>Nome</label>
+                        </Col>
+                        <Col>
+                            <label>Cargo</label>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col sm={4} >
+                            <label>123</label>
+                        </Col>
+                        <Col>
+                            <label>Rafael Floriano</label>
+                        </Col>
+                        <Col>
+                            <label>Programador</label>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col sm={4} >
+                            <label>124</label>
+                        </Col>
+                        <Col>
+                            <label>Sara Mi Amore</label>
+                        </Col>
+                        <Col>
+                            <label>Futura Medoca</label>
+                        </Col>
+                    </Row>
+                </Container>
+                <Container>
+                    <Row>
+                        <Col sm={4} >
+                            <label>125</label>
+                        </Col>
+                        <Col>
+                            <label>Gaúcho</label>
+                        </Col>
+                        <Col>
+                            <label>Aposentado</label>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         </div>
     ); 
